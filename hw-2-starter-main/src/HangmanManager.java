65349f5209a7134428f18e8f03030876f5e6b246
SamuelStevenJohnson <samuelsteven.johnson@calbaptist.edu>
2021-10-18 21:46:03 -0700

    Final Commit HW2

--- hw-2-starter-main/src/HangmanManager.java
+++ hw-2-starter-main/src/HangmanManager.java
@@ -1,6 +1,178 @@
+import java.util.*;
+
 /**
- * Add your comments here
+ *  Primary class for "Evil Hangman"
+ * Contains the primary logic for a game of hangman that cheats by always leaving the player with the widest possible
+ * set of answers when the guess a letter.
+ * @Author Christopher Nugent, Mikyung Han for EGR221 Solutions
+ * @Version Jan 2017
+ */
+
+/**
+ * worked on with Nathan, Jacob, and Adam
  */
 public class HangmanManager {
-    //Implement your code here
+
+    private Set<String> answerSpace;
+    private SortedSet<Character> guessedCharacters;
+    private int guessesRemaining;
+    private String pattern;
+
+    /*
+     * Constructor
+     * Initializes the HangmanManager from dictionary with words of length length. Allows a maximum of max guesses.
+     * Throws IllegalArgument exceptions if length is less than one or if max is less than zero.
+     * Parameters: dictionary of words to use, length of words to select, number of allowed incorrect guesses
+     */
+    public HangmanManager(Collection<String> dictionary, int length, int max) {
+        if(length < 1) {
+            throw new IllegalArgumentException("Length must be 1 or more.");
+        } else if (max < 0) {
+            throw new IllegalArgumentException("Maximum incorrect guesses must be more than 0.");
+        }
+
+        answerSpace = new TreeSet<>();
+        for(String word:dictionary) {
+            if (word.length() == length) {
+                answerSpace.add(word);
+            }
+        }
+
+        guessedCharacters = new TreeSet<>();
+        guessesRemaining = max;
+
+        pattern = "-";
+        for (int i = 1; i < length; i++) {
+            pattern += " -";
+        }
+    }
+
+    public Set<String> words() {
+        return new TreeSet<>(answerSpace);
+    }
+    public int guessesLeft() {
+        return guessesRemaining;
+    }
+    public SortedSet<Character> guesses() {
+        return new TreeSet<>(guessedCharacters);
+    }
+
+    public String pattern() {
+        if (answerSpace.isEmpty())
+            throw new IllegalStateException("answer space should not be empty");
+        return pattern;
+    }
+
+    /*
+     * Allows the user to guess a letter of the word and updates the state of HangmanManager accordingly.
+     * If there are no possible answers remaining, throws an IllegalStateException (no words were found).
+     * If there are not enough guessedCharacters remaining, throws an IllegalStateException.
+     * If the character has already been guessed, throws an IllegalArgumentException.
+     * Parameter: Letter being guessed
+     * Returns: Number of occurrences  of that letter in the pattern
+     */
+    public int record(char guess) {
+        if(answerSpace.size() == 0) {
+            throw new IllegalStateException("The set of possible answers is empty.");
+        } else if (guessesLeft() < 1) {
+            throw new IllegalStateException("No guesses remaining.");
+        } else if (guessedCharacters.contains(guess)) {
+            throw new IllegalArgumentException(guess + " has already been guessed.");
+        }
+
+        guessedCharacters.add(guess);
+
+        Map<String, Set<String>> answerMap = buildMap(guess);
+        int numOccur = updateAnswerInfo(answerMap, guess);
+
+        if (numOccur == 0) guessesRemaining--;
+        return numOccur;
+    }
+
+    /**
+     * Helper method for buildMap
+     * This method builds the new pattern of the word given the word and the guess and the current pattern
+     * The pattern will be the key for the map
+     * (e.g., say the current pattern is "_ _ _ l _"
+     * @param word (e.g. "apple")
+     * @param guess (e.g. "a")
+     * @return returns the pattern of the word (e.g. "a _ _ _ l _") based on current pattern and guess
+     */
+    private String buildKey(String word, char guess) {
+        StringBuilder sb = new StringBuilder(pattern);
+        for(int i = 0; i < word.length(); i++) {
+            if(word.charAt(i) == guess) {
+                sb.setCharAt(i * 2, guess);
+            }
+        }
+        return sb.toString();
+    }
+
+    /*
+     * Helper method for record()
+     * Generates a map of index combinations to sets of word that match those index combinations.
+     * Parameters: character being guessed
+     * Returns: map of index combinations to word sets
+     */
+    private Map<String, Set<String>> buildMap(char guess) {
+        Map<String, Set<String>> answerMap = new TreeMap<>();
+        for(String word:answerSpace) {
+            String key = buildKey(word, guess);
+            Set<String> value;
+            if(!answerMap.containsKey(key)) {
+                value = new TreeSet<>();
+            } else {
+                value = answerMap.get(key);
+            }
+            value.add(word);
+            answerMap.put(key, value);
+        }
+        return answerMap;
+    }
+
+    /*
+     * Helper method for record()
+     * Parameters: map to search, char for the current guess
+     * post: updates answerSpace, pattern to show to user,
+     * @return the number of occurrences of guess in the new pattern
+     */
+    private int updateAnswerInfo(Map<String, Set<String>> answerMap, char guess) {
+        String bestKey = findBestKey(answerMap);
+        answerSpace = answerMap.get(bestKey);
+        int numOccurrences = calcDiff(pattern, bestKey);
+        pattern = bestKey;
+        return numOccurrences;
+    }
+
+    /**
+     * Static helper method for calculating the number of different characters
+     * @param str1
+     * @param str2
+     * @return returns the number of different characters in str1 and str2
+     */
+    private static int calcDiff(String str1, String str2){
+        int numDiff = 0;
+        for(int i = 0; i < str1.length(); i++){
+            if(str1.charAt(i) != str2.charAt(i))
+                numDiff++;
+        }
+        return numDiff;
+    }
+
+    /*
+     * Helper method for updateAnswerInfo()
+     * Returns the key of the largest Set in a map
+     * Parameters: map to search
+     * Returns: key which refers to that set
+     */
+    private String findBestKey(Map<String, Set<String>> answerMap){
+        Set<String> keySet = answerMap.keySet();
+        String bestKey = keySet.iterator().next();
+        for(String key:keySet) {
+            if(answerMap.get(key).size() > answerMap.get(bestKey).size()) {
+                bestKey = key;
+            }
+        }
+        return bestKey;
+    }
 }
--- hw-2-starter-main/tst/HangmanManagerTest.java
+++ hw-2-starter-main/tst/HangmanManagerTest.java
@@ -1,10 +1,13 @@
-import org.junit.*;     // JUnit tools
+/**
+ * This project was worked on with Nathan, Jacob, and Adam.
+ */
 
+import org.junit.*;     // JUnit tools
 import java.util.*;     // Collections
 import java.io.*;       // File access
 
 /**
- * Modify this comment
+ * Junit tests for Hangman maneger
  */
 public class HangmanManagerTest {
 
@@ -18,19 +21,126 @@ public class HangmanManagerTest {
             }
             return dictionary;
         } catch(FileNotFoundException e) {
-            Assert.fail("Something went wrong.");      //Something went wrong
+            Assert.fail("Something went wrong.");
         }
-        /* Should never be reached. */
         return new HashSet<>();
     }
 
     /**
-     * Add comments
+     * tests to see if dictionary exists
      */
     @Test
     public void test1(){
-        //Implement your test code
+        try {
+            Scanner input = new Scanner(new File("hw-2-starter-main/dictionary5.txt"));
+            List<String> dictionary = new ArrayList<String>();
+            HangmanManager test = new HangmanManager(dictionary, 5, 5);
+            Assert.fail();
+        } catch (FileNotFoundException a) {
+            System.out.println("This dictionary doesn't exist.");
+        }
+
+
+    }
+    /**
+     * tests to see if guessesLeft is working
+     */
+    @Test
+    public void test2(){
+        List<String> dictionary = new ArrayList<String>();
+        HangmanManager a = new HangmanManager(dictionary, 5, 7);
+        Assert.assertEquals(a.guessesLeft(), 7);
+    }
+    /**
+     * tests to see if guessesLeft works with zero guesses
+     */
+    @Test
+    public void test3(){
+        List<String> dictionary = new ArrayList<String>();
+        HangmanManager a = new HangmanManager(dictionary, 5, 0);
+        Assert.assertEquals(a.guessesLeft(), 0);
+    }
+    /**
+     * tests to see if the correct amount of dashes are displayed for dictionary2.txt
+     */
+    @Test
+    public void test4() throws FileNotFoundException {
+        Scanner input = new Scanner(new File("hw-2-starter-main/dictionary2.txt"));
+        List<String> dictionary = new ArrayList<String>();
+        while(input.hasNext())
+            dictionary.add(input.next().toLowerCase());
+        HangmanManager test = new HangmanManager(dictionary, 4, 4);
+        Assert.assertEquals("- - - -", test.pattern());
+
+    }
+    /**
+     * tests to see if the correct amount of dashes are displayed for dictionary.txt
+     */
+    @Test
+    public void test5() throws FileNotFoundException {
+        Scanner input = new Scanner(new File("hw-2-starter-main/dictionary.txt"));
+        List<String> dictionary = new ArrayList<String>();
+        while(input.hasNext())
+            dictionary.add(input.next().toLowerCase());
+        HangmanManager test = new HangmanManager(dictionary, 7, 4);
+        Assert.assertEquals("- - - - - - -", test.pattern());
+
+    }
+    /**
+     * tests to see if record catches when letters are guessed twice from dictionary2.txt
+     */
+    @Test
+    public void test6() {
+        try{
+            Scanner input = new Scanner(new File("hw-2-starter-main/dictionary2.txt"));
+            List<String> dictionary = new ArrayList<String>();
+            while (input.hasNext())
+                dictionary.add(input.next().toLowerCase());
+            HangmanManager test = new HangmanManager(dictionary, 4, 3);
+            test.record('q');
+            test.record('q');
+            Assert.fail();
+        } catch (IllegalArgumentException | FileNotFoundException a) {
+            System.out.println("Letter was already guessed.");
+        }
+
+    }
+    /**
+     * tests to see if record catches when letters are guessed twice from dictionary.txt
+     */
+    @Test
+    public void test7() {
+        try{
+            Scanner input = new Scanner(new File("hw-2-starter-main/dictionary.txt"));
+            List<String> dictionary = new ArrayList<String>();
+            while (input.hasNext())
+                dictionary.add(input.next().toLowerCase());
+            HangmanManager test = new HangmanManager(dictionary, 7, 8);
+            test.record('q');
+            test.record('q');
+            Assert.fail();
+        } catch (IllegalArgumentException | FileNotFoundException a) {
+            System.out.println("Letter was already guessed.");
+        }
+
+    }
+    /**
+     * tests to see if record catches when there are no more guesses allowed
+     */
+    @Test
+    public void test8() {
+        try{
+            List<String> dictionary = new ArrayList<String>();
+            HangmanManager test = new HangmanManager(dictionary, 4, 2);
+            test.record('q');
+            test.record('w');
+            test.record('e');
+            Assert.fail();
+        } catch (IllegalStateException a) {
+            System.out.println("No more guesses allowed.");
+        }
+
     }
 
-    //Add more tests here
+
 }
